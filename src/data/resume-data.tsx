daf829e36a84f0677ae7e470912b242c7ed3da31
chueh <b10902076@csie.ntu.edu.tw>
2024-05-26 18:38:56 +0800

    change data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -8,7 +8,7 @@ export const RESUME_DATA = {
   location: "Taiwan",
   locationLink: "https://maps.app.goo.gl/hvh9kTr7z278B9Ae7",
   about:
-    "Student at National Taiwan University majored in Computer Science and Information Engineering",
+    "Student at National Taiwan University major in Computer Science and Information Engineering",
   summary:
     "",
   avatarUrl: "https://drive.google.com/a/g.ntu.edu.tw/thumbnail?id=1VbKRC073t_dflOUn22imdybCLDtJYK3E",
@@ -26,65 +26,62 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "國立臺灣大學",
-    //   school: "National Taiwan University",
-      degree: "資訊工程學系 三年級",
-    //   degree: "Bachelor's Degree in Computer Science and Information Engineering",
+      school: "National Taiwan University",
+      degree: "3rd Year, Computer Science and Information Engineering",
       start: "2021",
-      end: "迄今",
+      end: "Now",
     },
     {
-      school: "臺中市立臺中第一高級中等學校",
-    //   school: "Taichung Municipal Taichung First Senior High School",
-      degree: "普通科",
+      school: "Taichung Municipal Taichung First Senior High School",
+      degree: "General Subjects",
       start: "2018",
       end: "2021",
     },
   ],
   work: [
     {
-      company: "臺大身心障礙學生權益促進會",
+        company: "NTU Cycling Club",
+        link: "https://www.instagram.com/ntu_cycling_club",
+        badges: [],
+        title: "General Affairs",
+        //   logo: ,
+        start: "2022",
+        end: "2023",
+        description:
+            "Manage and maintain the club's bicycles, organize riding events, and lead members to complete \"Northern Approach to Wuling\", \"Suhua Highway\", and \"Hualien-Taitung Valley\" rides.",
+    },
+    {
+      company: "NTU Disabled Students' Rights Association",
       link: "https://www.facebook.com/NTUDSRA/",
       badges: [],
-      title: "總務 → 社長",
+      title: "Treasurer → President",
     //   logo: ,
       start: "2022",
       end: "2024",
       description:
-        "提供身心障礙學生相互交流之平台，並舉辦障礙議題講座，提升大眾對障礙者的認知",
+        "Provide a platform for students with disabilities to interact with each other, and organize lectures on disability issues to enhance the public's awareness of people with disabilities.",
     },
     {
-      company: "特殊教育推行委員會",
+      company: "Special Education Promotion Committee",
       link: "",
       badges: [],
-      title: "委員（學生代表）",
+      title: "Committee Member (Student Representative)",
     //   logo: ,
       start: "2023",
       end: "2024",
       description:
-        "代表身心障礙學生爭取學生權益，建構友善、無障礙的校園環境",
+        "Advocate for the rights of students with disabilities and create a friendly, accessible campus environment.",
     },
     {
-      company: "學生申訴評議委員會",
+      company: "Student Grievance Review Committee",
       link: "",
       badges: [],
-      title: "委員（身心障礙學生代表）",
+      title: "Committee Member (Disabled Student Representative)",
     //   logo: ,
       start: "2022",
       end: "2024",
       description:
-        "審議學生申訴案件",
-    },
-    {
-      company: "臺大單車社",
-      link: "https://www.instagram.com/ntu_cycling_club",
-      badges: [],
-      title: "庶務",
-    //   logo: ,
-      start: "2022",
-      end: "2023",
-      description:
-        "管理及維護社團單車，並舉辦騎乘活動，帶領社員完成「北進武嶺」、「蘇花公路」、「花東縱谷」",
+        "Review student grievance cases.",
     },
   ],
   skills: [
@@ -94,7 +91,7 @@ export const RESUME_DATA = {
     "Unix",
   ],
   languages: [
-    "多益英語檢定 915分",
+    "TOEIC 915",
   ],
   projects: [
     // {
